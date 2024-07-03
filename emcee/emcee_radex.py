4c63ff5834bf66b6279e5d35da50a3c36f16b125
Chentao Yang <yangcht@gmail.com>
2024-07-04 00:31:16 +0200

    improve python file

--- emcee/emcee_radex.py
+++ emcee/emcee_radex.py
@@ -18,21 +18,23 @@
 
 # autopep8 --ignore E26 emcee_radex.py
 import os
-# For runing the code on the clusters
-import sys
-import logging
-import _pickle as pickle  
-import warnings
+import pandas as pd
 import numpy as np
-from astropy.io import ascii
-from astropy import units as u
-from astropy.table import Table
+import multiprocessing
+import emcee
+import pickle
 from scipy.optimize import curve_fit, minimize
 from scipy.interpolate import interp1d
-
-# to match the cosmo in the CO paper
+import warnings
+from astropy.io import ascii
+from astropy.table import Table
+from astropy import units as u
 from astropy.cosmology import FlatLambdaCDM
-cosmo = FlatLambdaCDM(H0=67.8 * u.km / u.s / u.Mpc, Om0=0.308)
+import logging
+import corner
+
+# For the pyradex package (ensure it's installed in your environment)
+import pyradex
 
 import matplotlib
 # Define the fonts to make plots look consistent across different machines
@@ -44,90 +46,57 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
-import multiprocessing
-import emcee
-import corner
-import pyradex
-# import pyradex.fjdu
-
-kms = u.km / u.s
-Jykms = u.Jy * kms
-
+# Set up logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-ncpu = multiprocessing.cpu_count()
+# Initialize constants and cosmology
+kms = u.km / u.s
+Jykms = u.Jy * kms
+# to match the cosmo in the CO paper
+cosmo = FlatLambdaCDM(H0=67.8 * u.km / u.s / u.Mpc, Om0=0.308)
 
-#Assuming ortho to para ratio equals 3
-opr    = 3
-fortho = opr/(1+opr)
+opr = 3
+fortho = opr / (1 + opr)
 
+# Initialize pyradex
+R = pyradex.Radex(species='co', datapath="radex_moldata",
+                  density={'oH2': fortho * 10.**10.0, 'pH2': (1 - fortho) * 10.**10.0},
+                  column=10.0**6.0,
+                  temperature=20.0,
+                  tbackground=2.7315,
+                  deltav=1.0,
+                  escapeProbGeom='lvg')
 
-def model_lvg(Jup, p, R=None):
-    log_density, log_temperature, log_column, log_size = p
+# Function definitions
+def model_lvg(Jup, params, R=None):
+    log_density, log_temperature, log_column, log_size = params
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        R.set_params(density={'oH2':fortho*10.**log_density,'pH2':(1-fortho)*10.**log_density},
+        R.set_params(density={'oH2': fortho * 10.**log_density, 'pH2': (1 - fortho) * 10.**log_density},
                      column=10.**log_column,
                      temperature=10.**log_temperature)
-        R.run_radex(validate_colliders=False,
-                    reuse_last=True, reload_molfile=False)
-        result = R.source_line_surfbrightness # Do not use get_table()
-    intensity = (result[np.asarray(np.int_(Jup)) - 1] *
-                 (10.**log_size * u.sr) * (1. * kms)).to(Jykms)
+        R.run_radex(validate_colliders=False, reuse_last=True, reload_molfile=False)
+        result = R.source_line_surfbrightness
+    intensity = (result[np.asarray(np.int_(Jup)) - 1] * (10.**log_size * u.sr) * (1. * kms)).to(Jykms)
     return intensity.value
 
-
-def model_lvg_tau(p, R=None):
-    log_density, log_temperature, log_column, log_size = p
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        R.set_params(density={'oH2':fortho*10.**log_density,'pH2':(1-fortho)*10.**log_density},
-                     column=10.**log_column,
-                     temperature=10.**log_temperature)
-        R.run_radex(validate_colliders=False,
-                    reuse_last=True, reload_molfile=False)
-        result = R.tau # Do not use get_table()
-    return np.amax(result)
-
-
-def residual(p, R=None, Jup=None, flux=None, eflux=None):
-    model_flux = model_lvg(Jup, p, R)
-    return (flux - model_flux) / eflux
-
-
 def lnlike(p, Jup, flux, eflux, R=None):
-    """ likelihood function"""
+    """Likelihood function"""
     try:
         model_flux = model_lvg(Jup, p, R)
     except ValueError:
         return -np.inf
-    # Some nan sometimes....
     if np.any(np.isnan(model_flux)):
         return -np.inf
-    return -0.5 * (np.sum(((flux - model_flux)**2.0 / eflux**2.0)+np.log(eflux**2.0)))
-
+    return -0.5 * (np.sum(((flux - model_flux)**2.0 / eflux**2.0) + np.log(eflux**2.0)))
 
 def lnprior(p, bounds, R=None):
-    # 1. Check the boundaries
     """Uniform prior"""
-    if (np.any(p > bounds[:, 1])
-            or np.any(p < bounds[:, 0])
-            or np.any(p[2]-p[0] >= 17.5)     #see bounds from dv/dr, log(NCO/dv)-log(n_H2) < 17.5 
-            or np.any(p[2]-p[0] <= 10.0)):   #log(NCO/dv)-log(n_H2) > 10
-        logp = -np.inf
-    else:
-        logp = -np.sum(np.log10(bounds.dot([-1, 1]))) #a flat prior
-    # 2. add bounds for tau, should be < 100
-    #   try:
-    #       tau_max = model_lvg_tau(p, R)
-    #   except ValueError:
-    #       return -np.inf
-    #   
-    #   if (tau_max > 100):
-    #       return -np.inf
-    return logp
-
+    if (np.any(p > bounds[:, 1]) or np.any(p < bounds[:, 0])
+            or np.any(p[2] - p[0] >= 17.5) or np.any(p[2] - p[0] <= 10.0)):
+        return -np.inf
+    return -np.sum(np.log10(bounds.dot([-1, 1])))
 
 def lnprob(p, Jup, flux, eflux, bounds=None):
     lp = lnprior(p, bounds, R=R)
@@ -135,77 +104,64 @@ def lnprob(p, Jup, flux, eflux, bounds=None):
         return -np.inf
     return lp + lnlike(p, Jup, flux, eflux, R=R)
 
-
 def read_data(filename):
-    """Read data into a comprehensible panda frame"""
-    
-    ######################################################
-    #  Disadvantage here: only includes J_up = 11 here,  #
-    #  please manually add more if you have              #
-    #  J_up >= 12 CO lines                               #
-    ######################################################
-    
-    ascii_data = ascii.read(
-        filename, names=[
-            "SOURCE", "z", "D_L", "line_width",
-            "CO_J_1", "eCO_J_1", "CO_J_2", "eCO_J_2", "CO_J_3", "eCO_J_3",
-            "CO_J_4", "eCO_J_4", "CO_J_5", "eCO_J_5", "CO_J_6", "eCO_J_6",
-            "CO_J_7", "eCO_J_7", "CO_J_8", "eCO_J_8", "CO_J_9", "eCO_J_9",
-            "CO_J_10", "eCO_J_10", "CO_J_11", "eCO_J_11", "CI_1", "eCI_1",
-            "CI_2", "eCI_2"])
+    ##############################################################
+    #  This will match the table in ../table/flux.data.          #
+    #  Note that you can add as many CO transitions as you can.  #
+    #  But you will always need to have                          #
+    #     four columns for the flux and errors for               #
+    #     the lines of CI(1-0) and CI(2-1).                      #
+    ##############################################################
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-    pd = ascii_data.to_pandas()
-    pd = pd.set_index('SOURCE')
-    return pd.T
+    data_lines = []
+    for line in lines:
+        if not line.strip().startswith("#") and line.strip():
+            data_lines.append(line.strip())
 
+    first_data_line = data_lines[0].split()
+    num_columns = len(first_data_line)
+
+    fixed_columns = ["SOURCE", "z", "D_L", "line_width"]
+    num_dynamic_columns = (num_columns - 8) // 2
+
+    co_columns = []
+    for i in range(num_dynamic_columns):
+        co_columns.append(f"CO_J_{i+1}")
+        co_columns.append(f"eCO_J_{i+1}")
+
+    all_columns = fixed_columns + co_columns + ["CI_1", "eCI_1", "CI_2", "eCI_2"]
+
+    data = []
+    for line in data_lines:
+        split_line = line.split()
+        data.append(split_line)
+
+    if any(len(row) != len(all_columns) for row in data):
+        raise ValueError("Number of columns in data rows does not match the expected number of columns")
+
+    df = pd.DataFrame(data, columns=all_columns)
+
+    numerical_columns = all_columns[1:]
+    for col in numerical_columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    df.set_index('SOURCE', inplace=True)
+    return df.T
 
 def get_source(source, data):
     """Retrieve redshift and CO data for a particular source"""
-
-    # source = 'NCv1.143'
     z = data[source]['z']
     line_width = data[source]['line_width']
-    delta_v = 1 * kms  # do not care actually, fully degenerate with
-                       # the column density
-
-    # selecting only CO lines
-    keys = [key for key in data[source].keys()
-            if 'CO' in key and 'eCO' not in key]
+    keys = [key for key in data[source].keys() if 'CO' in key and 'eCO' not in key]
     CO_data = Table(np.asarray([(Jlow + 1, data[source][key], data[source]['e' + key])
-                                for Jlow, key in enumerate(keys)
-                                if np.isfinite(data[source][key])]),
-                    names=['Jup', 'flux', 'eflux'],
-                    dtype=[int, float, float])
-
+                                for Jlow, key in enumerate(keys) if np.isfinite(data[source][key])]),
+                    names=['Jup', 'flux', 'eflux'], dtype=[int, float, float])
     Jup = CO_data['Jup'].data
     flux = CO_data['flux'].data * Jykms
     eflux = CO_data['eflux'].data * Jykms
-
     return z, line_width, Jup, flux, eflux
-
-
-# the pyradex.Radex object MUST be declared at the module level for
-# lnprob to work properly
-#  ....  Setup the Radex parameters
-
-# Note that N_CO is totaly degenerated with deltav, so in
-# principle we should fit for N_CO/deltav
-R = pyradex.Radex(species='co', datapath="radex_moldata",
-                  density={'oH2':fortho*10.**10.0,'pH2':(1-fortho)*10.**10.0},
-                  column=10.0**6.0,
-                  temperature=20.0,
-                  tbackground=2.7315,
-                  deltav=1.0,
-                  escapeProbGeom='lvg')
-
-# R = pyradex.fjdu.Fjdu(species='co', datapath="radex_moldata",
-#                   density={'oH2':fortho*10.**10,'pH2':(1-fortho)*10.**10},
-#                   column=10**6,
-#                   temperature=20,
-#                   tbg=2.7315,
-#                   deltav=1,
-#                   escapeProbGeom='lvg')
-
 
 def replot(source):
 
@@ -246,12 +202,12 @@ def replot(source):
     f_inter_pemcee_max = interp1d(model_Jup, model_lvg(model_Jup, pemcee_max, R), kind='cubic')
     
     # plot the models onto the CO SLED
-    ax.plot(plot_Jup, f_inter_pmin(plot_Jup), label=r'$\mathrm{{\chi}^2}$, linestyle='--', color='#2B61DD')    
+    ax.plot(plot_Jup, f_inter_pmin(plot_Jup), label=r'$\mathrm{{\chi}^2}$', linestyle='--', color='#2B61DD')    
    #ax.plot(plot_Jup, f_inter_pemcee(plot_Jup), label=r'$\mathrm{median_{MCMC}}$', linestyle='--', color='#2B61DD')
     ax.plot(plot_Jup, f_inter_pemcee_max(plot_Jup), label=r'$\mathrm{MCMC}$', color='#FFA833')
     
-    # plot the 100 "good models" within the [16, 84] quartile
-    inds = np.random.randint(len(narrow_flatchain), size=100)
+    # plot the 200 "good models" within the [16, 84] quartile
+    inds = np.random.randint(len(narrow_flatchain), size=200)
     for ind in inds:
         sample = narrow_flatchain[ind]
         f_inter_pemcee_sample = interp1d(model_Jup, model_lvg(model_Jup, sample, R), kind='cubic')
@@ -300,31 +256,15 @@ def replot(source):
     print('-', n_c[2],'-', T_c[2],'-', N_c[2],'-', P_c[2])
     print("4max", '\n', pemcee_max)
 
-
 def main():
-
     data = read_data("../data/flux.dat")
-    
+
     if not os.path.exists("./single"):
         os.makedirs("./single")
 
-    for source in data.keys():
-    # if True:
-    #     source = 'NCv1.143'
-
-        logger.info('Processing {}'.format(source))
-
-        delta_v = 1 * kms  # do not care actually, fully degenerate
-                           # with the column density
-
-        # TODO: would it help to use the CI data ?
-        # If 2 lines probably yes, otherwise, must be degenerate with
-        # the abundance
-
-        # Retrieve data
+    for source in data.columns:
+        logger.info(f'Processing {source}')
         z, line_width, Jup, flux, eflux = get_source(source, data)
-
-        R.set_params(tbg=2.7315 * (1 + z))
 
         # Line : Jy km/s
         # 1 Jansky=1 Jy = 10e-26 W m-2 Hz-1 = 1e-23 erg s-1 cm-2 Hz-1
@@ -352,21 +292,10 @@ def main():
         # linewidth : Thermal + turbulent width (FWHM) of the lines.; Unit: km s-1. ; Allowed range: 10(-3) - 10(3)
         # Size : Unit sr
 
-        bounds = np.array([[-3, 13],    # log H2 density
-                           [2, 3000],   # Kinetic temperature
-                                        # (Collisions temperature of CO
-                                        # in radex files)
-                           [5, 25],     # log Column density
-                           [-20, -5]])  # log size
-                           
-        R_angle = ((7/(cosmo.angular_diameter_distance(z).value*1000.0))**2 * np.pi)*10
-        # Assuming, 7 kpc size and mu=10 lensing magnification
+        R.set_params(tbg=2.7315 * (1 + z))
         
-        # basic starting point
-        p0 = [5.6,     # np.log10(total_density)
-              24,      # temperature
-              18,      # 10**16.75*300, # np.log10(column)
-              np.log10(R_angle)]  # np.log10(size)
+        # Assuming, 7 kpc size and mu=10 lensing magnification
+        R_angle = ((7 / (cosmo.angular_diameter_distance(z).value * 1000.0)) ** 2 * np.pi) * 10
 
         # As suggested by https://arxiv.org/pdf/1212.5955.pdf
         # Further narrowing down according to Zhang et al, A&A 568, A122 (2014)
@@ -382,11 +311,11 @@ def main():
         # Additional constrains:
         # N_CO/(n_H2*Xco) < 2 R = 10 kpc (assuming disk is =<5 kpc, Xco=5e-5)
         #  -->   N_CO/n_H2 < 5e19, within the dv/dr range.
-        
+
         bounds = np.array([[2.0, 7.0],
                            [np.log10(2.7315 * (1 + z)), 3.0],
                            [15.5, 19.5],
-                           [np.log10(R_angle)-4, np.log10(R_angle)+4]])
+                           [np.log10(R_angle) - 4, np.log10(R_angle) + 4]])
 
         p0 = [  4.0,    # np.log10(total_density)
                 1.4,    # np.log10(temperature)
@@ -395,100 +324,77 @@ def main():
 
         # Simple curve_fit to find a starting point
         # Using lambda to get the R object through
+        p0 = np.clip(p0, bounds[:, 0], bounds[:, 1])
+
         opt_fun = lambda p, log_density, log_temperature, log_column, log_size: \
-            model_lvg(p, [log_density, log_temperature, log_column, 
-                          log_size], R=R)
-         
+            model_lvg(p, [log_density, log_temperature, log_column, log_size], R=R)
+
         try:
-            popt, pcov = curve_fit(opt_fun, Jup, flux.value,
-                                   sigma=eflux.value, p0=p0,
-                                   bounds=list(zip(*bounds)))
-            logger.info("    curve_fit : {}".format(popt))
+            popt, pcov = curve_fit(opt_fun, Jup, flux.value, sigma=eflux.value, p0=p0, bounds=list(zip(*bounds)))
+            logger.info(f"curve_fit : {popt}")
         except RuntimeError:
-            logger.warn("    curve_fit : failed")
+            logger.warning("curve_fit : failed")
             popt = p0
             pcov = None
 
-        # minimize do not work well far from the solution, so use the
-        # curve_fit solution to test the lnprob function...
         p = popt
-        nll = lambda p, Jup, flux, eflux, : - \
-            lnprob(p, Jup, flux, eflux, bounds)
-        result = minimize(nll, p,
-                          args=(Jup, flux.value, eflux.value),
-                          bounds=bounds)
+        nll = lambda p, Jup, flux, eflux: -lnprob(p, Jup, flux, eflux, bounds)
+        result = minimize(nll, p, args=(Jup, flux.value, eflux.value), bounds=bounds)
         pmin = result.x
-        logger.info("    minimize : {}".format(pmin))
+        logger.info(f"minimize : {pmin}")
 
-        # Do the heavy computation
         ndim = len(popt)
-        
         #################### Define the number of walkers here
-        nwalkers = 400       # 400 walkers
+        nwalkers = 100       # 100 walkers
         n_iter_burn = 100    # burning phase, number of iterations = 100
-        n_iter_walk = 1000   # walking phase, number of iterations = 1000
-        
+        n_iter_walk = 500    # walking phase, number of iterations = 500
+
         # Random starting positions
         pos = [popt + 1e-3 * np.random.randn(ndim) for i in range(nwalkers)]
-        
+
         # Multithread
         with multiprocessing.Pool() as pool:
             sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,
                                             args=(Jup, flux.value, eflux.value),
                                             kwargs={'bounds': bounds},
                                             pool=pool)
-            # Burning time
-            logger.info("    burning samples")
-            pos, prob, state = sampler.run_mcmc(pos, n_iter_burn)     
+            logger.info("burning samples")
+            pos, prob, state = sampler.run_mcmc(pos, n_iter_burn)
             sampler.reset()
-            # Sampling time
-            logger.info("    walking")
-            result = sampler.run_mcmc(pos, n_iter_walk)  
+
+            logger.info("walking")
+            result = sampler.run_mcmc(pos, n_iter_walk)
             pemcee = np.percentile(sampler.flatchain, [50], axis=0)[0]
 
         chain, lnprobability = sampler.chain, sampler.lnprobability
 
-        with open("./single/{}_bounds.pickle".format(source), 'wb') as pkl_file:
+        with open(f"./single/{source}_bounds.pickle", 'wb') as pkl_file:
             pickle.dump((source, z, bounds,
                          (Jup, flux, eflux), (popt, pcov), pmin, pemcee, (chain, lnprobability)),
                         pkl_file)
 
+        chain_plot = np.hstack((sampler.flatchain[:, [0, 1, 2]], sampler.flatchain[:, [0]] + sampler.flatchain[:, [1]]))
+        new_pmin = np.hstack((pmin[:3], pmin[0] + pmin[1]))
+        
         # Quick plot the model
         # replot(source)
         
-        # Print only the best fitted range of n_H2, T_kin, N_CO and Pressure
-        chain_plot = np.hstack((sampler.flatchain[:,[0,1,2]], sampler.flatchain[:,[0]]+sampler.flatchain[:,[1]]))
-        new_pmin = np.hstack((pmin[:3],pmin[0]+pmin[1]))  
-        
-        n_h2, T_kin, N_co, Pres = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), 
-                      list(zip(*np.percentile(chain_plot, [16, 50, 84], axis=0))))
-        
-        # Output the best fit
+        n_h2, T_kin, N_co, Pres = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+                                      list(zip(*np.percentile(chain_plot, [16, 50, 84], axis=0))))
+
         print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-        print("xxx:", source, '\n' "xxx: minimised results")
-        print("xxx:", new_pmin)
-        # Print the MCMC results for +/- 1 sigma range of the paremeters
+        print(f"xxx: {source}\nxxx: minimised results")
+        print(f"xxx: {new_pmin}")
         print("xxx: emcee results")
-        print("xxx:", "n_H2")
-        print("xxx:", n_h2)
-        print("xxx:", "T_kin")    
-        print("xxx:", T_kin)
-        print("xxx:", "N_CO/dv")     
-        print("xxx:", N_co)
-        print("xxx:", "P")      
-        print("xxx:", Pres)
+        print("xxx: n_H2")
+        print(f"xxx: {n_h2}")
+        print("xxx: T_kin")
+        print(f"xxx: {T_kin}")
+        print("xxx: N_CO/dv")
+        print(f"xxx: {N_co}")
+        print("xxx: P")
+        print(f"xxx: {Pres}")
 
 if __name__ == '__main__':
-
     plt.ion()
-    # plt.close('all')
     main()
-
-
-# See :
-# - https://arxiv.org/pdf/1602.01095.pdf
-# - https://arxiv.org/pdf/1401.2998.pdf
-# - https://arxiv.org/pdf/1212.5955.pdf
-# - https://arxiv.org/abs/0809.2337
-# plt.figure()
-# plt.plot(Jup, model[:8]/model[0])
